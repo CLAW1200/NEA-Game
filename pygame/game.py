e65063dbed42ac5c4ece652493c017ef8bc6e3e9
Claw1200 <carstenwood12@gmail.com>
2022-08-03 17:36:49 +0100

    init

---
+++ pygame/game.py
@@ -0,0 +1,22 @@
+import pygame
+
+class gameWindow():
+    def __init__(self):
+        self.window = pygame.display.set_mode((800,600))
+        self.caption = pygame.display.set_caption("Game")
+        self.clock = pygame.time.Clock()
+        self.fps = 60
+
+    def gameLoop(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            self.window.fill((0,0,0))
+            pygame.display.update()
+            self.clock.tick(self.fps)
+        pygame.quit()
+        quit()
+    
+    
